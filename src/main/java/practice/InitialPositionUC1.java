9e477afa0cba50892446ff3e7a1320afa8d2db54
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-03-28 12:14:39 +0530

    uc1 added

---
+++ src/main/java/practice/InitialPositionUC1.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.Scanner;
+
+public class InitialPositionUC1 {
+
+	public static void main(String[] args) 
+	{
+		int playerPosition = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter Player Name: ");
+        String playername = s.next();
+
+
+	}
+
+}
